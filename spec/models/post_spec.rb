67b39d8c327474a63d7db566ae8839670215e5ed
AlaaAlsalem <alsalem.aladdin@gmail.com>
2023-06-17 23:25:35 +0300

    Add test method for last five comment

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+
   # Test associations
   it { should belong_to(:author).class_name('User') }
   it { should have_many(:comments) }
@@ -10,6 +11,7 @@ RSpec.describe Post, type: :model do
   describe 'after_save' do
     let(:post) { create(:post) }
     let(:author) { post.author }
+    
 
     it 'should call update_posts_counter' do
       expect(post).to receive(:update_posts_counter)
@@ -37,6 +39,33 @@ RSpec.describe Post, type: :model do
       it 'should increment posts_counter of the author' do
         expect { post.send(:update_posts_counter) }.to change { author.reload.posts_counter }.by(1)
       end
+    end
+  end
+  describe 'recent_posts' do
+    before(:example) do
+      #@user = User.create(name: 'John Doe', photo: 'Person Image', bio: 'I am a teacher', posts_counter: 0)
+      @user = User.create(name: 'Tom', photo: 'https://picsum.photos/200/500', bio: 'Teacher from Mexico.')      
+      @post = Post.create(author: user, title: 'Hello', text: 'This is my first post')
+    end
+
+    let!(:comment1) do
+      Comment.create(text: 'Comment 1', author: @user, post: @post)
+    end
+    let!(:comment2) do
+      Comment.create(text: 'Comment 1', author: @user, post: @post)
+    end
+    let!(:comment3) do
+      Comment.create(text: 'Comment 3', author: @user, post: @post)
+    end
+    let!(:comment4) do
+      Comment.create(text: 'Comment 4', author: @user, post: @post)
+    end
+    let!(:comment5) do
+      Comment.create(text: 'Comment 5', author: @user, post: @post)
+    end
+
+    it 'should return recent 5 photos' do
+      expect(@post.five_latest_comments).to eq(5)
     end
   end
 end
