df6b7124aa1339f05f759724a80f22aae448e286
AlaaAlsalem <alsalem.aladdin@gmail.com>
2023-06-24 17:45:42 +0300

    Add Rspec testing to contrallers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'posts/posts'
-  get 'home/home'
-  root 'home#home'
+  root 'users#index'
 
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
   end
   # Defines the root path route ("/")
   # root "articles#index"
